2dae64f3d0c36850aeb91dd86d90249240fd281a
Fadele <fadeleadu@gmail.com>
2021-04-29 03:48:27 +0100

    Test app completed

---
+++ src/products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+      id: 1,
+      name: "Knife Set",
+      description: "4 sets of Kitchen knife",
+      catigory_id: "001",
+      created_date: "18-2-2019",
+      updated_date: "10-3-2021",
+      last_purchase_date: "11-3-2021"
+    },
+    {
+      id: 2,
+      name: "Microwave",
+      description: "Samsung microwave oven, with thriple functionality",
+      catigory_id: "002",
+      created_date: "1-1-2020",
+      updated_date: "3-3-2020",
+      last_purchase_date: "4-4-2021"
+    },
+    {
+      id: 3,
+      name: "Plate Rack",
+      description: "A Rack of chines designer plates",
+      catigory_id: "003",
+      created_date: "6-6-2020",
+      updated_date: "7-4-2021",
+      last_purchase_date: "2-2-2021"
+    },
+    {
+      id: 4,
+      name: "Knife Set",
+      description: "4 sets of Kitchen knife",
+      catigory_id: "001",
+      created_date: "18-2-2019",
+      updated_date: "10-3-2021",
+      last_purchase_date: "11-3-2021"
+    },
+    {
+      id: 5,
+      name: "Microwave",
+      description: "Samsung microwave oven, with thriple functionality",
+      catigory_id: "002",
+      created_date: "1-1-2020",
+      updated_date: "3-3-2020",
+      last_purchase_date: "4-4-2021"
+    },
+    {
+      id: 6,
+      name: "Plate Rack",
+      description: "A Rack of chines designer plates",
+      catigory_id: "003",
+      created_date: "6-6-2020",
+      updated_date: "7-4-2021",
+      last_purchase_date: "2-2-2021"
+    },
+    {
+        id: 7,
+        name: "Plate Rack",
+        description: "A Rack of chines designer plates",
+        catigory_id: "003",
+        created_date: "6-6-2020",
+        updated_date: "7-4-2021",
+        last_purchase_date: "2-2-2021"
+      }
+  ];
+  
+  export default products;
+  
